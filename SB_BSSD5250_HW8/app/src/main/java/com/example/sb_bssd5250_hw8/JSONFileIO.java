2ef634fe6e2e28569a50ab4274db7c55fe706e27
scottbing <sbing52@comcast.net>
2019-09-18 19:19:25 -0600

    SB_BSSD5230_HW8
    SB_BSSD5230_HW8

--- SB_BSSD5250_HW8/app/src/main/java/com/example/sb_bssd5250_hw8/JSONFileIO.java
+++ SB_BSSD5250_HW8/app/src/main/java/com/example/sb_bssd5250_hw8/JSONFileIO.java
@@ -15,11 +15,13 @@ public class JSONFileIO {
 
     public JSONFileIO() {
         //setContentView(R.layout.activity_main);
+        this.mAppContext = mAppContext.getApplicationContext();
+        
         makeData();
         //writeDataFile();
-        NoteJSONSerializer noteJSONSerializer = new NoteJSONSerializer( mAppContext, "notes.json" );
+        NoteJSONSerializer noteJSONSerializer = new NoteJSONSerializer( this.mAppContext, "notes.json" );
         try {
-            noteJSONSerializer.saveNotes (NotesData.getInstance(mAppContext).getNoteList());
+            noteJSONSerializer.saveNotes (NotesData.getInstance(this.mAppContext).getNoteList());
         }  catch (Exception e) {
             Log.d(LOGID, e.toString());
         }
@@ -28,7 +30,7 @@ public class JSONFileIO {
 
 
     private void makeData() {
-        ArrayList<Note> notes = NotesData.getInstance(mAppContext).getNoteList();
+        ArrayList<Note> notes = NotesData.getInstance(this.mAppContext).getNoteList();
         for(int i=0;  i<10;  i++) {
             Note note = new Note();
             note.setName("Note	#" + i);
@@ -38,9 +40,9 @@ public class JSONFileIO {
     }
 
     private void writeDataFile() {
-        ArrayList<Note> notes = NotesData.getInstance(mAppContext).getNoteList();
+        ArrayList<Note> notes = NotesData.getInstance(this.mAppContext).getNoteList();
         String filename = "notes.txt";
-        File file = new File(mAppContext.getFilesDir(), filename);
+        File file = new File(this.mAppContext.getFilesDir(), filename);
         FileOutputStream fileOutputStream;
         try {    //try to open the  file for writing
             fileOutputStream = new FileOutputStream(file);
@@ -56,7 +58,7 @@ public class JSONFileIO {
 
     private void readDataFile(String filename)  {
 
-        File file = new File(mAppContext.getFilesDir(), filename);
+        File file = new File(this.mAppContext.getFilesDir(), filename);
         int length = (int)file.length();
         Log.d(LOGID,"File is bytes: " + String.valueOf(length));
 
--- SB_BSSD5250_HW8/app/src/main/java/com/example/sb_bssd5250_hw8/MainActivity.java
+++ SB_BSSD5250_HW8/app/src/main/java/com/example/sb_bssd5250_hw8/MainActivity.java
@@ -1,21 +1,20 @@
 package com.example.sb_bssd5250_hw8;
 
-import androidx.annotation.NonNull;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity  implements NotesData.NotesDataUpdatedListener {
@@ -23,6 +22,7 @@ public class MainActivity extends AppCompatActivity  implements NotesData.NotesD
     private RecyclerView notesRV;
     private NotesAdapter notesAdapter;
     private int i = 1;
+    private static String LOGID = "MainActivity";
 
 
     @Override
@@ -58,11 +58,24 @@ public class MainActivity extends AppCompatActivity  implements NotesData.NotesD
         //make this button add a new note when clicked
         addNoteButton.setOnClickListener(addClickedListener);
 
+        Button saveNoteButton = new Button( this);
+        saveNoteButton.setText("Save");
+        saveNoteButton.setLayoutParams(new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,  ViewGroup.LayoutParams.WRAP_CONTENT));
+        //make this button save a new note when clicked
+        saveNoteButton.setOnClickListener(saveClickedListener);
+
+        // got this from the instructor
         LinearLayout linearLayout = new LinearLayout( this);
+        LinearLayout buttonLL =  new  LinearLayout(this);
+        buttonLL.setOrientation(LinearLayout.HORIZONTAL);
+        buttonLL.addView(addNoteButton);
+        buttonLL.addView(saveNoteButton);
+
         linearLayout.setOrientation(LinearLayout.VERTICAL);
         linearLayout.setLayoutParams(new LinearLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,  ViewGroup.LayoutParams.MATCH_PARENT));
-        linearLayout.addView(addNoteButton);
+        linearLayout.addView(buttonLL);
         linearLayout.addView(notesRV);
 
         setContentView(linearLayout);
@@ -84,16 +97,61 @@ public class MainActivity extends AppCompatActivity  implements NotesData.NotesD
         }
     };
 
-    /*@Override
-    public void onBindViewHolder(@NonNull NotesAdapter.ViewHolder holder, int position) {
+    private View.OnClickListener saveClickedListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Log.d("saveClick Listener", "add clicked");
+            NoteJSONSerializer noteJSONSerializer = new NoteJSONSerializer( MainActivity.this, "notes.json" );
+            try {
+                noteJSONSerializer.saveNotes (NotesData.getInstance(MainActivity.this).getNoteList());
+            }  catch (Exception e) {
+                Log.d(LOGID, e.toString());
+            }
+            readDataFile("notes json");
+        }
+    };
 
-        //get the singleton of all notes data, get teh array in it, get the item at position
-        Note note = NotesData.getInstance(MainActivity.mContext).getNoteList().get(position);
-        //the holder the adapter already made for this item is now populated
-        holder.nameView.setText(note.getName());
-        holder.dateView.setText(note.getDate());
-        holder.descView.setText(note.getDesc());
-        holder.position = position;
+    private void writeDataFile() {
+        ArrayList<Note> notes = NotesData.getInstance(this).getNoteList();
+        String filename = "notes.txt";
+        File file = new File(getApplicationContext().getFilesDir(), filename);
+        FileOutputStream fileOutputStream;
+        try {    //try to open the  file for writing
+            fileOutputStream = new FileOutputStream(file);
+            //Turn the first note's name to bytes   in utf8 format and put in file
+            fileOutputStream.write(notes.get(0).toString().getBytes("UTF-8"));
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (Exception e) {
+            //catch any errors that occur from try and throw them back to whoever called this
+            Log.d(LOGID, e.toString());
+        }
+    }
 
-    }*/
+    private void readDataFile(String filename)  {
+
+        File file = new File(getApplicationContext().getFilesDir(), filename);
+        int length = (int)file.length();
+        Log.d(LOGID,"File is bytes: " + String.valueOf(length));
+
+        byte[] bytes = new byte[length]; //byte array to hold all read bytes
+
+        FileInputStream fileInputStream;
+        try { //try to open the file for reading
+            fileInputStream = new FileInputStream(file);
+            fileInputStream.read(bytes);
+            fileInputStream.close();
+        } catch (Exception e) {//handle exception arising from above
+            Log.d(LOGID, e.toString());
+        }
+
+        //Now try something different that also requires eception handling
+        try {
+            String s = new String(bytes,"UTF-8");
+            Log.d(LOGID, s);
+        } catch (Exception e) { //handle exception from string creation
+            Log.d(LOGID, e.toString());
+        }
+    }
+
 }
